8b8c7fc036fc8dc21d0461457a396ea3b425b1f7
omniboyOk <kachorritou@gmail.com>
2019-12-28 02:44:23 -0300

    server stuff

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const request = require('request-promise-native')
 const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
-const apikey = process.env.API_KEY;
 app.use(favicon(__dirname + "/build/favicon.ico"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
@@ -20,7 +19,7 @@ app.get("/", function (req, res) {
 app.get("/api/", async (req, res) => {
   //paso los datos de categoria a la api
   let categoria = req.query.categoria;
-  const uri = `https://api.nytimes.com/svc/topstories/v2/${categoria}.json?api-key=${apikey}`;
+  const uri = `https://api.nytimes.com/svc/topstories/v2/${categoria}.json?api-key=bxGlWsCJOETPGjfULoUPDvt3RBclFgCn`;
   //llamo a la api del diario
   const api_res = await request(uri)
     .catch(err => {
